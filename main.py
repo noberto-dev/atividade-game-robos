b9fada4a472f2b0688be76adc031839ddea7674d
kazydev-bit <isaacdacosta2022@gmail.com>
2025-12-10 14:15:55 -0300

    mudei o caminho dos assets

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     
     TELA = pygame.display.set_mode((LARGURA, ALTURA))
     pygame.display.set_caption("Robot Defense - Template")
-    Fundo = pygame.image.load('atividade-game-robos-main/assets/mapa1.png')
+    Fundo = pygame.image.load('assets\mapa1.png')
     FPS = 60
     clock = pygame.time.Clock()
 
--- tiro.py
+++ tiro.py
@@ -5,7 +5,7 @@ from entidade import *
 class Tiro(Entidade):
     def __init__(self, x, y):
         super().__init__(x + 43, y, 10)
-        self.image = pygame.image.load("atividade-game-robos-main/assets/tiro.png")
+        self.image = pygame.image.load("assets/tiro.png")
         self.image = pygame.transform.scale(self.image, (20,20))
 
     def update(self):
