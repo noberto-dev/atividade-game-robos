932f9aa4cde55a3fdb26b21d4fc9de5d644cee37
kazydev-bit <isaacdacosta2022@gmail.com>
2025-12-11 12:09:03 -0300

    implementação das animações RoboZigueZague

--- robo_ziguezague.py
+++ robo_ziguezague.py
@@ -5,20 +5,19 @@ from config import LARGURA, ALTURA
 
 class RoboZigueZague(Robo):
     def __init__(self, x, y):
-        super().__init__(x, y, velocidade = 5)
+        super().__init__(x, y, velocidade=5)
         self.direcao = 1
 
     def atualizar_posicao(self):
         self.rect.y += self.velocidade
         self.rect.x += self.direcao * 3
 
-        if self.rect.x <= 0 or self.rect.x >= LARGURA - 40:
+        if self.rect.x <= 0 or self.rect.x >= LARGURA - self.rect.width:
             self.direcao *= -1
 
-    def update(self,):
-        self.atualizar_posicao()
         if self.rect.y > ALTURA:
             self.kill()
+
 
 
 
